123a1e5a74e3aa2e1c020b3d1999167e2e1b2a80
RudiJr <rudi.juniior@gmail.com>
2020-08-16 02:01:26 -0300

    Adição de novas Telas

---
+++ price_list.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="../Projeto Integrador II/estilo/css/price_style.css" rel="stylesheet">
+    <title>Preços</title>
+  </head>
+
+  <body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal">Nome da companhia</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Premium</a>
+        <a class="p-2 text-dark" href="#">Suporte</a>
+        <a class="p-2 text-dark" href="#">Preços</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Cadastro</a>
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Preços</h1>
+      <p class="lead">Construa uma tabela de preços para seus potenciais clientes, com esse exemplo Bootstrap. Além do mais, é feito com componentes padrões, utilitários e um pouquinho de customização.</p>
+    </div>
+
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Grátis</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mês</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 usuários</li>
+              <li>2 GB de armazenamento</li>
+              <li>Suporte por email</li>
+              <li>Acesso ao centro de ajuda</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Cadastre-se de graça</button>
+          </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mês</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>20 usuários</li>
+              <li>10 GB de armazenamento</li>
+              <li>Suporte por email prioritário</li>
+              <li>Acesso ao centro de ajuda</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Conhecer</button>
+          </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Premium</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mês</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>30 usuários</li>
+              <li>15 GB de armazenamento</li>
+              <li>Suporte por email e telefone</li>
+              <li>Acesso ao centro de ajuda</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Contate-nos</button>
+          </div>
+        </div>
+      </div>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="../../assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; Copyright: Rudi Júnior - GTI 2020</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Algo legal</a></li>
+              <li><a class="text-muted" href="#">Feature aleatória</a></li>
+              <li><a class="text-muted" href="#">Recursos para times</a></li>
+              <li><a class="text-muted" href="#">Coisas para desenvolvedores</a></li>
+              <li><a class="text-muted" href="#">Outra coisa legal</a></li>
+              <li><a class="text-muted" href="#">Último item</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Fontes</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Fonte</a></li>
+              <li><a class="text-muted" href="#">Nome da fonte</a></li>
+              <li><a class="text-muted" href="#">Outra fonte</a></li>
+              <li><a class="text-muted" href="#">Fonte final</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Sobre</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Equipe</a></li>
+              <li><a class="text-muted" href="#">Locais</a></li>
+              <li><a class="text-muted" href="#">Privacidade</a></li>
+              <li><a class="text-muted" href="#">Termos</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
